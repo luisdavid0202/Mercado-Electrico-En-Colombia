147bec3f652487563dbb16a0c40b06602a61ea39
Miguel Angel Monsalve Arango <50241610+migvelmonsalve@users.noreply.github.com>
2020-07-07 18:01:15 -0500

    Create transform.py
    Initial commit

---
+++ src/download_and_process_data/transform.py
@@ -0,0 +1,136 @@
+# Standard library imports
+import pandas as pd
+import time
+import os
+
+# Local application/library imports
+import setup
+
+start = time.time()
+
+parent_dir = os.getcwd()
+path_download_files = os.path.join(parent_dir, setup.DATA_DIRECTORY_NAME)
+
+db = pd.DataFrame(
+    columns=[
+        "DEPARTAMENTO",
+        "MUNICIPIO",
+        "AGENTE COMERCIALIZADOR",
+        "NIVEL TENSION",
+        "DIA",
+        "DEMANDA",
+        "PRECIO",
+    ]
+)
+
+months = {
+    'enero': 1,
+    'febrero': 2,
+    'marzo': 3,
+    'abril':4,
+    'mayo':5,
+    'junio':6,
+    'julio':7,
+    'agosto':8,
+    'septiembre':9,
+    'octubre':10,
+    'noviembre':11,
+    'diciembre':12
+    }
+
+count_input = 0
+
+for filename in os.listdir(path_download_files):
+
+    count_input += 1
+    print(f"[INFO] Procesing a file named '{filename}' {count_input}/{len(os.listdir(path_download_files))}")
+
+    file = pd.read_excel(os.path.join(path_download_files, filename), sheet_name=0, header=None)
+    
+    positions = list()
+    result = file.isin(["DEPARTAMENTO"])
+    columns = result.any()
+    true_columns = list(columns[columns == True].index)
+
+    for col in true_columns:
+
+        rows = list(result[col][result[col] == True].index)
+
+        for row in rows:
+            positions.append((row, col))
+
+    file = file.iloc[positions[0][0] :, positions[0][1] :]
+    new_header = list(file.iloc[0])
+    new_header2 = [
+        str(col)
+        .replace("Í", "I")
+        .replace("Ó", "O")
+        .replace("\n", "")
+        .replace(" DE ", " ")
+        .upper()
+        for col in new_header
+    ]
+    file = file[1:]
+    file.columns = new_header2
+    file = file[
+        [
+            "DEPARTAMENTO",
+            "MUNICIPIO",
+            "AGENTE COMERCIALIZADOR",
+            "NIVEL TENSION",
+            "CONCEPTO",
+        ]
+        + [col for col in file.columns if "DIA" in str(col)]
+    ].reset_index(drop=True)
+
+    file = pd.melt(
+        file,
+        id_vars=[
+            "DEPARTAMENTO",
+            "MUNICIPIO",
+            "AGENTE COMERCIALIZADOR",
+            "NIVEL TENSION",
+            "CONCEPTO",
+        ],
+        value_vars=[col for col in file.columns if "DIA" in str(col)],
+        var_name="DIA",
+    )
+
+    file["DIA"] = file.apply(lambda x: x["DIA"].replace("DIA ", ""), axis=1)
+
+    file = file.pivot_table(
+        index=[
+            "DEPARTAMENTO",
+            "MUNICIPIO",
+            "AGENTE COMERCIALIZADOR",
+            "NIVEL TENSION",
+            "DIA",
+        ],
+        columns="CONCEPTO",
+        values="value",
+        aggfunc="first",
+    ).reset_index()
+
+    file.rename(
+        columns={
+            "Demanda Diaria GWh": "DEMANDA",
+            "Precio Promedio Diario ($/kWh)": "PRECIO",
+        },
+        inplace=True,
+    )
+
+    file["FECHA"] = (
+        int("20" + filename.split(".")[0][-2:]) * 100
+        + months[filename.split(".")[0].split("_")[0].lower()]
+    ) * 100 + file["DIA"].astype(int)
+
+    db = db.append(file, ignore_index=True)
+
+db.to_csv('data.csv',index=False)
+
+end = time.time()
+
+if end >= 60:
+    print("[INFO] Elapsed time on execution: {:.2f} minutes...".format((end - start) / 60))
+else:
+    print("[INFO] Elapsed time on execution: {:.2f} seconds...".format(end - start))
